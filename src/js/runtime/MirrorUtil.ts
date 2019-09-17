7af8835f0df9e330d9f6bfeb85b3de4cbb282fc1
meixg <meixg@foxmail.com>
2019-09-17 17:41:25 +0800

    fix: js runtime set array

--- src/js/runtime/MirrorUtil.ts
+++ src/js/runtime/MirrorUtil.ts
@@ -5,11 +5,16 @@ export function get() {
 export function set(obj: any, path: string[], value: any) {
     let target = obj;
     path.forEach((key, index) => {
-        if (!target[key] || typeof target[key] !== 'object') {
-            target[key] = {};
-        }
         if (index === path.length - 1) {
             target[key] = value;
+        }
+        else if (!target[key] || typeof target[key] !== 'object') {
+            if (isNaN(+path[index + 1])) {
+                target[key] = {};
+            }
+            else {
+                target[key] = [];
+            }
         }
         target = target[key];
     });
